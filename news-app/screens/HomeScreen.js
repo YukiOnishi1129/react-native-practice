1c7607baea078cf986aa1197a1ad770dc2c558cd
大西雄貴 <yuki.onishi1129@gmail.com>
2020-11-25 00:25:18 +0900

    Loadingなど実装

--- news-app/screens/HomeScreen.js
+++ news-app/screens/HomeScreen.js
@@ -3,20 +3,24 @@ import { StyleSheet, FlatList, SafeAreaView } from 'react-native'
 import ListItem from '../components/ListItem'
 import Constants from 'expo-constants'
 import axios from 'axios'
+import Loading from '../components/Loading'
 
 const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
 // const URL = `https://asia-northeast1-news-app-udemy.cloudfunctions.net/dummy_news`
 
 export default HomeScreen = ({ navigation }) => {
   const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
     fetchArticles()
   }, [])
 
   const fetchArticles = async () => {
+    setLoading(true)
     try {
       const response = await axios.get(URL)
       setArticles(response.data.articles)
+      setLoading(false)
     } catch (error) {
       console.error(error)
     }
@@ -40,6 +44,7 @@ export default HomeScreen = ({ navigation }) => {
         )}
         keyExtractor={(item, index) => index.toString()}
       />
+      {loading && <Loading />}
     </SafeAreaView>
   )
 }
