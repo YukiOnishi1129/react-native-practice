3e3ffe05d381a688cfb0b61f015014678412adc1
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-07 00:45:25 +0900

    スクロールイベント

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useRef } from 'react';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import md5 from 'md5';
 
 const styles = StyleSheet.create({
+  container: {
+    marginTop: 44,
+  },
   label: {
+    // width: 200
     height: 400,
   },
   separator: {
@@ -12,20 +16,33 @@ const styles = StyleSheet.create({
   },
 });
 
+interface Item {
+  id: string;
+  title: string;
+}
+
 const data = [
   { id: 'first', title: 'ひとつめ' },
   { id: 'second', title: 'ふたつめ' },
   { id: 'third', title: 'みっつめ' },
+  { id: 'fourth', title: '四つめ' },
+  { id: 'fifth', title: '5つめ' },
+  { id: 'sixth', title: '6つめ' },
+  { id: 'seventh', title: '7つめ' },
 ];
 
 export default function App() {
-  console.log(data);
+  const list = useRef<FlatList<Item>>(null);
+  const scrollToThird = useCallback(() => {
+    list?.current?.scrollToIndex({ index: 2 });
+  }, [list]);
+
   return (
-    <FlatList
-      data={data}
-      renderItem={({ item }) => <Text style={styles.label}>{item.title}</Text>}
-      ItemSeparatorComponent={() => <View style={styles.separator} />}
-      keyExtractor={item => md5(JSON.stringify(item))}
-    />
+    <View style={styles.container}>
+      <TouchableOpacity onPress={scrollToThird}>
+        <Text>Scroll to third</Text>
+      </TouchableOpacity>
+      <FlatList ref={list} data={data} renderItem={({ item }) => <Text style={styles.label}>{item.title}</Text>} />
+    </View>
   );
 }
