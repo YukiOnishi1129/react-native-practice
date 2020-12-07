035528e6c5260c9be730c835a7028688465da92f
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-07 23:49:02 +0900

    Alert

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import {
   FlatList,
   StyleSheet,
@@ -9,6 +9,7 @@ import {
   TextInput,
   StatusBar,
   ActivityIndicator,
+  Alert,
 } from 'react-native';
 import md5 from 'md5';
 // import useControlledComponent from './lib/hooks';
@@ -18,7 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black',
   },
   button: {
     backgroundColor: 'red',
@@ -35,10 +35,43 @@ const styles = StyleSheet.create({
   },
 });
 
+const useErrorModalDialog = (initialErrors: Array<string>) => {
+  const [needsToShow, setNeedsToShow] = React.useState(true);
+  const [errors, setErrors] = useState(initialErrors);
+
+  useEffect(() => {
+    if (errors.length > 0 && needsToShow) {
+      setNeedsToShow(false);
+      Alert.alert(errors[0], undefined, [
+        {
+          text: 'OK',
+          onPress: () => {
+            setErrors(errors.filter((_, index) => index !== 0));
+            setNeedsToShow(true);
+          },
+        },
+      ]);
+    }
+  }, [needsToShow, errors]);
+
+  const addError = (newError: string) => {
+    setErrors([...errors, newError]);
+  };
+
+  return addError;
+};
+
 export default function App() {
+  const addError = useErrorModalDialog(['1st', '2nd', '3rd']);
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="small" color="white" />
+      <TouchableOpacity
+        onPress={() => {
+          addError('new error');
+        }}
+      >
+        <Text>generate error</Text>
+      </TouchableOpacity>
     </View>
   );
 }
