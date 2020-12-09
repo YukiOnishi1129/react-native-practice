429150b5538358495bc5340c596a89b416037e3c
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-10 00:27:34 +0900

    Stack

--- LearnRN/src/index.tsx
+++ LearnRN/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {useNavigation, NavigationContainer} from '@react-navigation/native'
 
 const styles = StyleSheet.create({
   container: {
@@ -9,8 +11,49 @@ const styles = StyleSheet.create({
   },
 })
 
+const Main = () => {
+  const {navigate} = useNavigation()
+  return (
+    <View style={styles.container}>
+      <Text>Main</Text>
+      <TouchableOpacity
+        onPress={() => {
+          // ここで画面遷移
+          navigate('Sub')
+        }}>
+        <Text>go to sub</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const Sub = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sub</Text>
+    </View>
+  )
+}
+
+// createStackNavigatorで作成したインスタンスを用いてルーティングを定義
+const Stack = createStackNavigator()
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Sub" component={Sub} />
+    </Stack.Navigator>
+  )
+}
+
 const Index = () => {
-  return <View style={styles.container} />
+  return (
+    //   NavigationContainer:ルーティングを束ねておくコンテナ
+    <NavigationContainer onStateChange={(newState) => console.log(newState)}>
+      <StackNavigator />
+    </NavigationContainer>
+  )
 }
 
 export default Index
