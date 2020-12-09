c9d2fca1a8c23b116b8facc55e6f62a29a9c2b14
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-09 23:03:39 +0900

    事前準備完了

---
+++ LearnRN/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+})
+
+const Index = () => {
+  return <View style={styles.container} />
+}
+
+export default Index
