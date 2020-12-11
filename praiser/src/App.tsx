5932f1a04c074bbe8fed7d73ec07dfc1b28738fa
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-12 02:41:26 +0900

    事前準備

---
+++ praiser/src/App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+declare const global: { HermesInternal: null | {} };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default App;
