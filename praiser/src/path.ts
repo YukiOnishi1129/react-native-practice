a8c7a75cb32f5c318c886fb86b9129c86ab4c187
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-12 20:46:42 +0900

    画面名を定数化

---
+++ praiser/src/path.ts
@@ -0,0 +1,11 @@
+// 画面名の定義
+export const INITIAL = 'INITIAL'; // 初回起動時の画面
+export const HOME = 'HOME'; // ホーム画面
+export const DETAIL = 'DETAIL'; // Todo詳細画面
+export const SIGN_UP = 'SIGN_UP'; // サインアップ画面
+export const SIGN_IN = 'SIGN_IN'; // サインイン画面
+export const USER_INFO = 'USER_INFO'; // ユーザー情報画面
+export const STATISTICS = 'STATISTICS'; // 統計情報画面
+export const LOADING = 'LOADING'; // ロード画面
+export const CHOOSE_LOGIN = 'CHOOSE_LOGIN'; // サインイン、サインアウト選択画面
+export const INPUT = 'INPUT'; // Todo登録画面
