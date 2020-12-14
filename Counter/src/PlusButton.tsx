379c80db454919b569889e67821f664192750724
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-14 23:50:00 +0900

    コンポーネント分割

---
+++ Counter/src/PlusButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#008080',
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
+
+interface Props {
+  count: number;
+  setCounter: (count: number) => void;
+}
+
+export default function PlusButton(props: Props) {
+  const {count, setCounter} = props;
+  const addCounter = React.useCallback(() => {
+    setCounter(count + 1);
+  }, [count, setCounter]);
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={addCounter}>
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+  );
+}
--- Counter/src/index.tsx
+++ Counter/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import CounterText from './CounterText';
+import PluButton from './PlusButton';
+import ReduceButton from './ReduceButton';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,54 +11,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
   },
-  button: {
-    backgroundColor: '#008080',
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-  },
-  disabledButton: {
-    backgroundColor: 'gray',
-  },
-  textContainer: {
-    width: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-  },
 });
 
 export default function Counter() {
   const [count, setCounter] = React.useState(0);
-  const addCounter = React.useCallback(() => {
-    setCounter(count + 1);
-  }, [count]);
-  const reduceCounter = React.useCallback(() => {
-    setCounter(count - 1);
-  }, [count]);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={addCounter}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
-      <View style={styles.textContainer}>
-        {count >= 10 && <Text>you're great!</Text>}
-        <Text style={styles.text}>{count}</Text>
-      </View>
-      <TouchableOpacity
-        style={[styles.button, count <= 0 && styles.disabledButton]}
-        onPress={reduceCounter}
-        disabled={count <= 0}>
-        <Text style={styles.buttonText}>-</Text>
-      </TouchableOpacity>
+      {/* ＋ボタン */}
+      <PluButton count={count} setCounter={setCounter} />
+      {/* counterテキスト表示 */}
+      <CounterText count={count} />
+      {/* -ボタン */}
+      <ReduceButton count={count} setCounter={setCounter} />
     </View>
   );
 }
