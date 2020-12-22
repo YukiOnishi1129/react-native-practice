c050610a428ad29ff69f0bf607e1d0da5a516e2c
yuki <yuki.onishi1129@gmail.com>
2020-12-23 01:07:29 +0900

    ログイン画面

--- shop-review/shop-review-app/src/navigation/AppNavigator.tsx
+++ shop-review/shop-review-app/src/navigation/AppNavigator.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 /* navigator */
 import { MainTabNavigator } from "./MainTabNavigator";
+/* screens */
+import { AuthScreen } from "../screens/AuthScreen";
 
 export const AppNavigator = () => {
+  const user = { id: "12" };
   return (
     <NavigationContainer>
       {/* スクリーンを表示させる */}
       {/* MainTabNavigator：下に表示されるタブを表示 */}
-      <MainTabNavigator />
+      {!user ? <AuthScreen /> : <MainTabNavigator />}
     </NavigationContainer>
   );
 };
