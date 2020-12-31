fcb06eb29fa4bbc60fee2d8103d5e1bd4508873a
yuki <yuki.onishi1129@gmail.com>
2020-12-31 20:07:28 +0900

    レビュー一覧表示

--- shop-review/shop-review-app/src/screens/ShopScreen.tsx
+++ shop-review/shop-review-app/src/screens/ShopScreen.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
+import { getReviews } from "../lib/firebase";
 /* components */
 import { ShopDetail } from "../components/ShopDetail";
 import { FloatingActionButton } from "../components/FloatingActionButton";
+import { ShopReviewItem } from "../components/ShopReviewItem";
 /* type */
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../types/navigation";
+import { Review } from "../types/review";
+import { ReviewItem } from "../components/ReviewItem";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "Shop">;
@@ -16,13 +20,25 @@ type Props = {
 export const ShopScreen: React.FC<Props> = ({ navigation, route }) => {
   // Home画面から渡ってきたshopのデータを取得
   const { shop } = route.params;
+  const shopId = !!shop && shop.id ? shop.id : "";
+  const [reviews, setReviews] = useState<Review[]>([]);
   useEffect(() => {
     navigation.setOptions({ title: shop.name }); // ヘッダータブのタイトルに店名を表示させる
+    const fetchReviews = async () => {
+      const reviews = await getReviews(shopId);
+      setReviews(reviews);
+    };
+    fetchReviews();
   }, [shop]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <ShopDetail shop={shop} />
+      <FlatList
+        ListHeaderComponent={<ShopDetail shop={shop} />}
+        data={reviews}
+        renderItem={({ item }) => <ReviewItem review={item} />}
+        keyExtractor={(item) => item.id}
+      />
       <FloatingActionButton
         iconName="plus"
         onPress={() => navigation.navigate("CreateReview", { shop })}
