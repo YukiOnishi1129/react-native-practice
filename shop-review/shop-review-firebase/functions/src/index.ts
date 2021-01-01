0e5fdcd343a3f50af39ea93e5a02d6d78d2df14a
yuki <yuki.onishi1129@gmail.com>
2021-01-01 19:11:17 +0900

    ユーザー名を各レビューに反映する (collection groupとバッチ処理を使用)

--- shop-review/shop-review-firebase/functions/src/index.ts
+++ shop-review/shop-review-firebase/functions/src/index.ts
@@ -1,9 +1,45 @@
 import * as functions from "firebase-functions";
+import admin = require("firebase-admin");
+import { UserInitail } from "./types/user";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// 最初にfirebaseの処理を初期化
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+exports.onUpdateUser = functions
+  .region("asia-northeast1")
+  // どのドキュメントが更新されたかを定義 (更新された後に非同期で実行)
+  .firestore.document("users/{userId}")
+  // onUpdate: すでに存在するドキュメントの値が変更されたときにトリガーされます。
+  // https://firebase.google.com/docs/functions/firestore-events
+  .onUpdate(async (change, context) => {
+    const { userId } = context.params;
+    // 更新後のuserId change.after.data()で更新前のデータを取得できる
+    const newUser = change.after.data() as UserInitail;
+
+    //   revirewドキュメントの更新処理
+    const db = admin.firestore();
+    try {
+      // コレクショングループ
+      // ドキュメントを一括検索する
+      // https://firebase.google.com/docs/firestore/query-data/queries#collection-group-query
+      const snapshot = await db
+        .collectionGroup("reviews")
+        .where("user.id", "==", userId)
+        .get();
+
+      // reviewsドキュメントの更新
+      // バッチ処理で更新
+      // https://firebase.google.com/docs/firestore/manage-data/transactions#batched-writes
+      const batch = db.batch();
+      snapshot.docs.forEach((reviewDoc) => {
+        //   「...reviewDoc.data().user」で既存のデータを展開し、name: newUser.nameでユーザーの名前だけを上書きする
+        const user = { ...reviewDoc.data().user, name: newUser.name };
+        //   reviewsドキュメントを更新
+        batch.update(reviewDoc.ref, { user });
+      });
+      // 最後に処理を確定させる
+      await batch.commit();
+    } catch (err) {
+      console.log(err);
+    }
+  });
