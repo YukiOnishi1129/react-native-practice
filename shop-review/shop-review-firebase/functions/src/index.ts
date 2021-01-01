6b460aa417610faf444e5625025c088f956bce58
yuki <yuki.onishi1129@gmail.com>
2021-01-02 00:35:21 +0900

    push通知

--- shop-review/shop-review-firebase/functions/src/index.ts
+++ shop-review/shop-review-firebase/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
 import algoliasearch from "algoliasearch";
+import { Expo, ExpoPushMessage } from "expo-server-sdk";
 import { UserInitail } from "./types/user";
 import { Review } from "./types/review";
 import { Shop } from "./types/shop";
@@ -15,6 +16,9 @@ const index = client.initIndex("reviews"); // Algliaで作成した「reviews」
 
 // 最初にfirebaseの処理を初期化
 admin.initializeApp();
+
+// Expo初期化
+let expo = new Expo();
 
 exports.onUpdateUser = functions
   .region("asia-northeast1")
@@ -131,4 +135,64 @@ exports.onWriteReview = functions
     } catch (err) {
       console.log(err);
     }
+  });
+
+/**
+ * push通知用
+ */
+exports.scheduledFunctionCrontab = functions
+  .region("asia-northeast1")
+  .pubsub.schedule("*/2 * * * *")
+  .timeZone("Asia/Tokyo")
+  .onRun(async (context) => {
+    const snapshot = await admin.firestore().collection("users").get();
+    const pushTokens = snapshot.docs
+      .map((doc) => (doc.data() as UserInitail).pushToken)
+      .filter((token) => !!token);
+
+    // Create the messages that you want to send to clients
+    let messages: ExpoPushMessage[] = [];
+    for (let pushToken of pushTokens) {
+      // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+      // Check that all your push tokens appear to be valid Expo push tokens
+      if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`);
+        continue;
+      }
+
+      // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+      messages.push({
+        to: pushToken,
+        sound: "default",
+        body: "週末に行ったレストランのレビューを書こう♪",
+        data: { withSome: "data" },
+      });
+    }
+
+    // The Expo push notification service accepts batches of notifications so
+    // that you don't need to send 1000 requests to send 1000 notifications. We
+    // recommend you batch your notifications to reduce the number of requests
+    // and to compress them (notifications with similar content will get
+    // compressed).
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+      // Send the chunks to the Expo push notification service. There are
+      // different strategies you could use. A simple one is to send one chunk at a
+      // time, which nicely spreads the load out over time:
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          console.log(ticketChunk);
+          tickets.push(...ticketChunk);
+          // NOTE: If a ticket contains an error code in ticket.details.error, you
+          // must handle it appropriately. The error codes are listed in the Expo
+          // documentation:
+          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
   });
